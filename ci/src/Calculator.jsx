b529f800d38320b5e68ae0a381420178af062702
Kumar Adarsh <kradarsh24jan@gmail.com>
2025-08-12 13:33:38 +0530

    add lint facility to code

--- ci/src/Calculator.jsx
+++ ci/src/Calculator.jsx
@@ -1,7 +1,10 @@
+import PropTypes from "prop-types";
 import React from "react";
-const Calculator = ( {arr} ) => {
-  return <div>
-    {arr[0]+arr[1]}
-  </div>
+const Calculator = ({ arr }) => {
+  return <div>{arr[0] + arr[1]}</div>;
 };
 export default Calculator;
+
+Calculator.propTypes = {
+  arr: PropTypes.array.isRequired,
+};
--- ci/vitest.config.js
+++ ci/vitest.config.js
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 console.log("vitest config loaded");
 
